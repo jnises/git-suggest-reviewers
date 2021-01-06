fe526ea6474b1aad5ab5dcce1f607cac2de4d570
Joel Nises <joel.nises@gmail.com>
2021-01-06 19:10:04 +0100

    diff

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
-use anyhow::Result;
+use anyhow::{Result, anyhow, Context};
 use structopt::StructOpt;
-use git2::Repository;
+use git2::{Repository, DiffFormat};
 
 #[derive(Debug, StructOpt)]
 #[structopt(about = "List authors of lines changed by PR")]
 struct Opt {
-    /// where to merge from
-    #[structopt()]
-    compare: String,
-
     /// where to merge to
     #[structopt()]
     base: String,
+
+    /// where to merge from
+    #[structopt()]
+    compare: String,
 }
 
 fn main() -> Result<()> {
     let opt = Opt::from_args();
     let repo = Repository::open(".")?;
-    println!("{:?}", repo.head()?.name());
+    let base = repo.find_reference(&opt.base).context("unable to find base")?.resolve()?;
+    let base_oid = base.target().ok_or_else(|| anyhow!("cannot find base {:?}", base.name()))?;
+    let compare = repo.find_reference(&opt.compare)?.resolve()?;
+    let compare_oid = compare.target().ok_or_else(|| anyhow!("cannot find compare {:?}", base.name()))?;
+    let merge_base = repo.merge_base(base_oid, compare_oid)?;
+    let merge_base_tree = repo.find_tree(merge_base)?;
+    println!("merge base: {:?}", merge_base_tree);
+    let diff = repo.diff_tree_to_tree(Some(&merge_base_tree), Some(&compare.peel_to_tree()?), None)?;
+    diff.print(DiffFormat::Patch, |delta, hunk, line| {
+        println!("{:?} {:?} {:?}", delta, hunk, line);
+        true
+    })?;
     Ok(())
 }
