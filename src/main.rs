46b177f055992e2684da8c363d4520b7732d056c
Joel Nises <joel.nises@gmail.com>
2021-01-06 18:38:42 +0100

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use structopt::StructOpt;
+use git2::Repository;
+
+#[derive(Debug, StructOpt)]
+#[structopt(about = "List authors of lines changed by PR")]
+struct Opt {
+    /// where to merge from
+    #[structopt()]
+    compare: String,
+
+    /// where to merge to
+    #[structopt()]
+    base: String,
+}
+
+fn main() -> Result<()> {
+    let opt = Opt::from_args();
+    let repo = Repository::open(".")?;
+    println!("{:?}", repo.head()?.name());
+    Ok(())
 }
