e11d8ec36451f97eb22e386de5c2f73ab99d4908
joel nises <joel.nises@gmail.com>
2021-01-09 18:31:46 +0100

    more functionality

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use anyhow::{anyhow, Context, Result};
-use git2::{Blame, DiffDelta, DiffFormat, DiffHunk, DiffLine, Oid, Repository};
-use log::{info, warn};
-use std::path::PathBuf;
+use git2::{Blame, BlameOptions, DiffDelta, DiffFormat, DiffHunk, DiffLine, Oid, Repository};
+use log::{debug, error, info, warn};
+use std::{collections::HashMap, path::PathBuf};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -28,9 +28,9 @@ fn main() -> Result<()> {
     let opt = Opt::from_args();
     env_logger::builder()
         .filter_level(if opt.verbose {
-            log::LevelFilter::Info
+            log::LevelFilter::Debug
         } else {
-            log::LevelFilter::Error
+            log::LevelFilter::Warn
         })
         .init();
     let repo = Repository::open(".")?;
@@ -45,11 +45,13 @@ fn main() -> Result<()> {
         info!("merge base: {:?}", merge_base);
     }
     let diff = repo.diff_tree_to_tree(Some(&merge_base_tree), Some(&compare_tree), None)?;
+    // TODO need to do some rename detection on the diff?
     // diff.print(DiffFormat::Patch, |delta, hunk, line| {
     //     println!("{:?} {:?} {:?}", delta, hunk, line);
     //     true
     // })?;
     // println!("diff: {:?}", diff.stats()?);
+    let mut modified: HashMap<(Option<String>, Option<String>), usize> = HashMap::new();
     let mut blame_cache: Option<(PathBuf, Result<Blame, git2::Error>)> = None;
     diff.foreach(
         // file_cb
@@ -64,25 +66,64 @@ fn main() -> Result<()> {
         // binary_cb
         None,
         // hunk_cb
-        None,
-        // line_cb
-        Some(
-            &mut |delta: DiffDelta, _hunk: Option<DiffHunk>, line: DiffLine| {
-                if let Some(path) = delta.old_file().path() {
-                    if blame_cache.is_none() || blame_cache.as_ref().unwrap().0 != path {
-                        let newblame = repo.blame_file(path, None);
-                        if let Err(ref e) = newblame {
-                            warn!("error blaming {:?}: {}", path, e);
-                        }
-                        blame_cache = Some((path.to_path_buf(), newblame));
+        Some(&mut |delta: DiffDelta, hunk: DiffHunk| {
+            if let Some(path) = delta.old_file().path() {
+                if blame_cache.is_none() || blame_cache.as_ref().unwrap().0 != path {
+                    let newblame =
+                        repo.blame_file(path, Some(BlameOptions::new().newest_commit(merge_base)));
+                    if let Err(ref e) = newblame {
+                        warn!("error blaming {:?}: {}", path, e);
                     }
-                    if let Ok(blame) = &blame_cache.as_ref().unwrap().1 {
-                        print!("{}", String::from_utf8_lossy(line.content()));
-                    }
+                    blame_cache = Some((path.to_path_buf(), newblame));
                 }
-                true
-            },
-        ),
+                if let Ok(blame) = &blame_cache.as_ref().unwrap().1 {
+                    for line in hunk.old_start()..(hunk.old_start() + hunk.old_lines()) {
+                        if let Some(oldhunk) = blame.get_line(line as usize) {
+                            let sign = oldhunk.final_signature();
+                            let author = (
+                                sign.name().map(|s| String::from(s)),
+                                sign.email().map(|s| String::from(s)),
+                            );
+                            modified.entry(author).and_modify(|e| *e += 1).or_insert(1);
+                        // let commit_oid = oldhunk.final_commit_id();
+                        // if let Ok(commit) = repo.find_commit(commit_oid) {
+                        //     commit.author()
+                        // } else {
+                        //     warn!("cannot find commit with id {}", commit_oid);
+                        // }
+                        } else {
+                            debug!("line {} not found in {:?}@{}", line, path, merge_base);
+                        }
+                    }
+                    // let offset = line.content_offset();
+                    // blame.
+                    // print!("{}", String::from_utf8_lossy(line.content()));
+                }
+            }
+            true
+        }),
+        // line_cb
+        None,
+        // Some(
+        //     &mut |delta: DiffDelta, _hunk: Option<DiffHunk>, line: DiffLine| {
+        //         if let Some(path) = delta.old_file().path() {
+        //             if blame_cache.is_none() || blame_cache.as_ref().unwrap().0 != path {
+        //                 let newblame = repo.blame_file(path, Some(BlameOptions::new().newest_commit(merge_base)));
+        //                 if let Err(ref e) = newblame {
+        //                     warn!("error blaming {:?}: {}", path, e);
+        //                 }
+        //                 blame_cache = Some((path.to_path_buf(), newblame));
+        //             }
+        //             if let Ok(blame) = &blame_cache.as_ref().unwrap().1 {
+        //                 let offset = line.content_offset();
+        //                 blame.
+        //                 print!("{}", String::from_utf8_lossy(line.content()));
+        //             }
+        //         }
+        //         true
+        //     },
+        // ),
     )?;
+    print!("modified: {:?}", modified);
     Ok(())
 }
